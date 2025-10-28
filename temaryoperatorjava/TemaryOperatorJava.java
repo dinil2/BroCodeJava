3e229bf0d9b3f7cd994e7d17eb2bb415f446d2e0
Dinil Bhashana <155037594+dinil2@users.noreply.github.com>
2025-10-28 20:08:15 +0530

    Add files via upload

---
+++ temaryoperatorjava/TemaryOperatorJava.java
@@ -0,0 +1,66 @@
+package com.mycompany.temaryoperatorjava;
+
+public class TemaryOperatorJava {
+
+    public static void main(String[] args) {
+        //ternary operator ? = Return 1 of 2 values if a condition is true
+        
+        // variable = (condition) ? ifTrue : ifFalse
+        
+        int score = 75;
+        
+        String passOrFail = (score >= 60) ? "PASS" : "FAIL";
+        
+        System.out.println(passOrFail);
+        
+        
+    }
+}
+
+
+
+
+
+
+/* package com.mycompany.temaryoperatorjava;
+
+public class TemaryOperatorJava {
+
+    public static void main(String[] args) {
+        //ternary operator ? = Return 1 of 2 values if a condition is true
+        
+        // variable = (condition) ? ifTrue : ifFalse
+        
+        int number = 3;
+        
+        Sring evenOrOdd = (number % 2 == 0) ? "Even" : "Odd"; 
+        
+
+        System.out.println(evenOrOdd);
+        
+        
+    }
+}
+
+*/
+
+/* package com.mycompany.temaryoperatorjava;
+
+public class TemaryOperatorJava {
+
+    public static void main(String[] args) {
+        //ternary operator ? = Return 1 of 2 values if a condition is true
+        
+        // variable = (condition) ? ifTrue : ifFalse
+        
+        int hours = 13;
+        
+        String timeOfDay = (hours < 12) ? "AM" : "PM";
+        
+        System.out.println(hours);
+        
+        
+    }
+}
+
+*/
